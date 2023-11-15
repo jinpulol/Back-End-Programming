57c1d82e8860fb20a33af0ea62f3c91b0c51a90b
jinpulol <jesselehmonen@gmail.com>
2023-11-15 03:30:53 +0200

    more changes

--- src/main/java/sof03/lfg/web/CategoryController.java
+++ src/main/java/sof03/lfg/web/CategoryController.java
@@ -1,10 +1,49 @@
 package sof03.lfg.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-// import org.springframework.ui.Model;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import sof03.lfg.domain.Category;
+import sof03.lfg.domain.CategoryRepository;
+
+// Controller for categories
 @Controller
 public class CategoryController {
+
+    // Injecting repositories
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    // Show all categories
+    @GetMapping("/categories")
+    public String categories(Model model) {
+        model.addAttribute("categories", categoryRepository.findAll());
+
+        return "categorylist";
+    }
+    
+    // Add new category
+    @GetMapping("/addcategory")
+    public String addCategory(Model model) {
+        model.addAttribute("category", new Category());
+        return "addcategory";
+    }
+
+    // Save new category
+    @PostMapping("/savecategory")
+    public String saveCategory(Category category) {
+        categoryRepository.save(category);
+        return "redirect:categories";
+    }
+
+    // Delete category
+    @GetMapping("/deletecategory/{id}")
+    public String deleteCategory(Long categoryId, Model model) {
+        categoryRepository.deleteById(categoryId);
+        return "redirect:../categories";
+    }
+    
 }
--- src/main/java/sof03/lfg/web/EventController.java
+++ src/main/java/sof03/lfg/web/EventController.java
@@ -4,28 +4,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import sof03.lfg.domain.CategoryRepository;
+import sof03.lfg.domain.Meeting;
 import sof03.lfg.domain.MeetingRepository;
 
 
-
+// Controller for meetings
 @Controller
 public class EventController {
 
+    // Injecting repositories
     @Autowired
     private MeetingRepository meetingRepository;
 
     @Autowired
     private CategoryRepository categoryRepository;
     
-
+    // Index page
     @GetMapping("/")
     public String index() {
         return "index";
     }
 
+    // Show all meetings
     @GetMapping("/events")
     public String events(Model model) {
         model.addAttribute("meetings", meetingRepository.findAll());
@@ -33,5 +37,34 @@ public class EventController {
         return "eventlist";
     }
 
+    // Add new meeting
+    @GetMapping("/add")
+    public String addMeeting(Model model) {
+        model.addAttribute("meeting", new Meeting());
+        model.addAttribute("categories", categoryRepository.findAll());
+        return "addmeeting";
+    }
+
+    // Save new meeting
+    @PostMapping("/save")
+    public String saveMeeting(Meeting meeting) {
+        meetingRepository.save(meeting);
+        return "redirect:events";
+    }
+
+    // Delete meeting
+    @GetMapping("/delete/{id}")
+    public String deleteMeeting(@PathVariable("id") Long meetingId, Model model) {
+        meetingRepository.deleteById(meetingId);
+        return "redirect:../events";
+    }
+
+    // Edit meeting
+    @GetMapping("/edit/{id}")
+    public String editMeeting(@PathVariable("id") Long meetingId, Model model) {
+        model.addAttribute("meeting", meetingRepository.findById(meetingId));
+        model.addAttribute("categories", categoryRepository.findAll());
+        return "editmeeting";
+    }
 
 }

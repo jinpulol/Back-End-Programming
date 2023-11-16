73085b4407d4e30ce46a6db479826d1029ecf804
jinpulol <jesselehmonen@gmail.com>
2023-11-16 05:06:48 +0200

    allow H2

--- src/main/java/sof03/lfg/WebSecurityConfig.java
+++ src/main/java/sof03/lfg/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 import sof03.lfg.web.UserDetailServiceImpl;
 
@@ -26,18 +27,27 @@ public class WebSecurityConfig {
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
         http
-            .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers(antMatcher("/css/**")).permitAll() // Enable css when logged out
-                .requestMatchers(antMatcher("/")).permitAll() // Allow access to index page without authentication
-                 .requestMatchers(antMatcher("/register")).permitAll() // Allow access to register page without authentication
-                 .requestMatchers(antMatcher("/events")).permitAll() // Allow access to events page without authentication")))
-                .anyRequest().authenticated())
-            .formLogin(formLogin -> formLogin
-                .loginPage("/login")
-                .defaultSuccessUrl("/events", true) // Redirect to events page after login
-                .permitAll())
-            .logout(logout -> logout
-                .permitAll());
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(antMatcher("/css/**")).permitAll() // Enable css when logged out
+                        .requestMatchers(antMatcher("/")).permitAll() // Allow access to index page without
+                                                                      // authentication
+                        .requestMatchers(antMatcher("/register")).permitAll() // Allow access to register page without
+                                                                              // authentication
+                        .requestMatchers(antMatcher("/events")).permitAll() // Allow access to events page without
+                                                                            // authentication")))
+                        .requestMatchers(toH2Console()).permitAll()
+                        .anyRequest().authenticated())
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers(toH2Console()))
+                .headers(headers -> headers
+                        .frameOptions(frameoptions -> frameoptions
+                                .disable()))
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/events", true) // Redirect to events page after login
+                        .permitAll())
+                .logout(logout -> logout
+                        .permitAll());
 
         return http.build();
     }
@@ -46,5 +56,5 @@ public class WebSecurityConfig {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
-    
+
 }
